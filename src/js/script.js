21f635f484b31e6e3ed528ca77e93f045118e514
Andrew <wotriger@gmail.com>
2026-02-25 18:36:33 +0100

    Add new section and swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
+import Swiper from 'swiper'
+import 'swiper/css'
+import { Autoplay } from 'swiper/modules'
 import '/src/sass/style.scss'
+
+const swiper = new Swiper('.swiper', {
+	modules: [Autoplay],
+	autoplay: {
+		delay: 3000, // 3 секунди
+		disableOnInteraction: false,
+	},
+	loop: true,
+	slidesPerView: 1,
+	spaceBetween: 80,
+})
